af85853b63fb147714a2dbe5bd0b9cfa2194adf5
Lucas Börjesson <lucas.ri.borjesson@gmail.com>
2026-09-16 13:12:51 +0200

    Update paddign on title layout

--- frontend/lib/layouts/page_title.tsx
+++ frontend/lib/layouts/page_title.tsx
@@ -16,7 +16,7 @@ export default function PageTitleLayout({
 }: PageTitleLayoutProps) {
   return (
     <DefaultLayout scroll={scroll}>
-        <div className="flex justify-between items-center mt-7 sm:mt-0">
+        <div className="flex justify-between items-center px-1 mt-7 sm:px-0 sm:mt-0">
           {title &&
             <h2 className="text-4xl font-semibold sm:text-4xl text-foreground">{title}</h2>
           }
